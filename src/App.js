027d6c9e31c62f31ea561490c30e47dea7beaa20
Sachin <nishadsachin398@gmail.com>
2024-05-17 17:21:17 +0530

    What the Shit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import './App.css'
 function App() {
   return (
     <div className='container'>
-    <h2>Mallah</h2>
-    <p>Sachin</p>
-   
-    <p>hello</p>
+    <p>What the Shit</p>
     </div>
   );
 }
